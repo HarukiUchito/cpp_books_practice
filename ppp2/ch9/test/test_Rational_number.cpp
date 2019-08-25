c9df304f32eb44220b904bccb705c491b188f4f9
Haruki Uchiito <uhcahriuiktio0114@outlook.com>
2019-08-25 17:38:56 +0900

    add test of operator=

--- ppp2/ch9/test/test_Rational_number.cpp
+++ ppp2/ch9/test/test_Rational_number.cpp
@@ -8,7 +8,13 @@ TEST(RationalNumberTest, INITIALIZEATION)
     EXPECT_DOUBLE_EQ(0.3, Rational_number(3, 10).to_double());
 }
 
-TEST(RationalNUmberTest, ADDITION)
+TEST(RationalNumberTest, ASSIGNMENT)
+{
+    Rational_number r = Rational_number {1, 2};
+    EXPECT_DOUBLE_EQ(0.5, r.to_double());
+}
+
+TEST(RationalNumberTest, ADDITION)
 {
     Rational_number r1 {1, 2}, r2 {1, 3};
     Rational_number sum {r1 + r2};
